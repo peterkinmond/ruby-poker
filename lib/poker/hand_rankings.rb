b901b579e10d5b99c04783c85218b60672066953
Peter Kinmond <peterkinmond@gmail.com>
2015-11-24 13:14:09 -0800

    Refactor hand rankings
    * Reduce if branch nesting and go with simpler logic
    * Add logic for 4 of a kind

--- lib/poker/hand_rankings.rb
+++ lib/poker/hand_rankings.rb
@@ -14,36 +14,65 @@ class HandRankings
   # 1  High Card
 
   def self.get_rank(hand)
-    if flush?(hand)
-      if straight?(hand)
-        if hasAce?(hand)
-          Rank.new(10, "Royal Flush")
-        else
-          Rank.new(9, "Straight Flush")
-        end
-      else
-        Rank.new(6, "Flush")
-      end
+    if royal_flush?(hand)
+      Rank.new(10, "Royal Flush")
+    elsif straight_flush?(hand)
+      Rank.new(9, "Straight Flush")
+    elsif four_of_a_kind?(hand)
+      Rank.new(8, "Four of a Kind")
+    #elsif full_house?(hand)
+      #Rank.new(7, "Full House")
+    elsif flush?(hand)
+      Rank.new(6, "Flush")
     elsif straight?(hand)
       Rank.new(5, "Straight")
+    #elsif three_of_a_kind?(hand)
+      #Rank.new(4, "Three of a Kind")
+    #elsif two_pair?(hand)
+      #Rank.new(3, "Two Pair")
+    #elsif one_pair?(hand)
+      #Rank.new(2, "One Pair")
     else
       Rank.new(1, "High Card")
     end
   end
 
-  def self.hasAce?(hand)
+  def self.royal_flush?(hand)
+    flush?(hand) && straight?(hand) && has_ace?(hand)
+  end
+
+  def self.straight_flush?(hand)
+    flush?(hand) && straight?(hand)
+  end
+
+  def self.four_of_a_kind?(hand)
+    ordered_card_values = hand.cards.map{|card| card.number}.sort
+
+    card_count = {}
+    ordered_card_values.each do |val|
+      if card_count.has_key?(val)
+        card_count[val] += 1
+        return true if card_count[val] == 4
+      else
+        card_count[val] = 1
+      end
+    end
+    return false
+  end
+
+  def self.has_ace?(hand)
     # TODO: Get rid of magic number
     hand.cards.any?{|card| card.number == 14}
   end
 
   def self.flush?(hand)
-    suit = hand.cards[0].suit
+    suit = hand.cards.first.suit
     hand.cards.all?{|card| card.suit == suit}
   end
 
   def self.straight?(hand)
     ordered_card_values = hand.cards.map{|card| card.number}.sort
-    first_num = ordered_card_values[0]
+    first_num = ordered_card_values.first
     ordered_card_values.each_with_index do |val, index|
       if first_num + index != val
         return false
--- tests/test_hand_rankings.rb
+++ tests/test_hand_rankings.rb
@@ -29,7 +29,15 @@ class TestHandRankings < Test::Unit::TestCase
   end
 
   def test_four_of_a_kind
-    # TODO: complete this
+    card1 = Card.new(5, 'Hearts')
+    card2 = Card.new(5, 'Diamonds')
+    card3 = Card.new(5, 'Clubs')
+    card4 = Card.new(5, 'Spades')
+    card5 = Card.new(8, 'Spades')
+    hand = Hand.new(card1, card2, card3, card4, card5)
+    rank = HandRankings.get_rank(hand)
+    assert_equal(8, rank.value)
+    assert_equal("Four of a Kind", rank.name)
   end
 
   def test_full_house
@@ -83,6 +91,5 @@ class TestHandRankings < Test::Unit::TestCase
     assert_equal(1, rank.value)
     assert_equal("High Card", rank.name)
   end
-
 
 end
