cd5dd21453276824b3ae2bab67cdcae57400a65b
Peter Kinmond <peterkinmond@gmail.com>
2015-11-23 22:26:02 -0800

    Hand object will hold all cards for a player

--- lib/poker/hand.rb
+++ lib/poker/hand.rb
@@ -3,4 +3,9 @@ class Hand
   # and 5 community cards
   # Include them in this class or add to player?
 
+  attr_reader :cards
+
+  def initialize(*cards)
+    @cards = cards
+  end
 end
--- tests/test_hand.rb
+++ tests/test_hand.rb
@@ -1,3 +1,12 @@
-class TestHand < Test::Unit::TestCase
+require "./lib/poker/hand.rb"
+require "./lib/poker/card.rb"
+require "test/unit"
 
+class TestHand < Test::Unit::TestCase
+  def test_hand_creation
+    card1 = Card.new(5, 'Spade')
+    card2 = Card.new(7, 'Spade')
+    hand = Hand.new(card1, card2)
+    assert_equal(2, hand.cards.size)
+  end
 end
